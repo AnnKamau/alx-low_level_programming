7a77949e61fa132b1cf4b2e3a628677e2a285461
AnnKamau <3annfaithk@gmail.com>
2023-07-14 01:22:51 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabetic
+ * Return: Always (Successful)
+ */
+int main(void)
+{
+	char z;
+
+	for (z = 'a'; z <= 'z'; z++)
+	putchar(z);
+	putchar('\n');
+	return (0);
+}

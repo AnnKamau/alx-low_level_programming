7c0579a19713bcaf0a0c7553e7ffa63f1bd8560d
AnnKamau <3annfaithk@gmail.com>
2023-07-14 02:47:25 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - alphabet backwards
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}

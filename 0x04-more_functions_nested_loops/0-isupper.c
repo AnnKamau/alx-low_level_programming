eeba3f50199e0c6dbd71d1aa8c5f946260d1fc24
AnnKamau <3annfaithk@gmail.com>
2023-07-20 01:51:55 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Program that checks uppercase characters
+ * Return: 0 (Successful)
+ */
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}

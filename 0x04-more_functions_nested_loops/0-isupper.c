b35fa9adbdf2dd1a3069a5c4955fee878fe742a2
AnnKamau <3annfaithk@gmail.com>
2023-07-20 02:02:46 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * main - Program that checks uppercase characters
- * Return: 0 (Successful)
+ * _isupper - Program that checks uppercase characters
+ * @c: the character to be checked
+ * Return: 1 for uppercase or 0 for anything else
  */
 int _isupper(int c)
 {
 
 	if (c >= 65 && c <= 90)
 	{
+
 	return (1);
 	}
-	else
-	{
 	return (0);
-	}
 }

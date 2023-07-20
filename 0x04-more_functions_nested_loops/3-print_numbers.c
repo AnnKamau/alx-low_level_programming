e67a667f373b299eb9f843e68090332aef2ebf24
AnnKamau <3annfaithk@gmail.com>
2023-07-20 03:24:18 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
 
 void print_numbers(void)
 {
-	int x;
+	int num;
 
-	for (x = 0; x <= 9; x++)
+	for (num = 0; num <= 9; num++)
 	{
-	_putchar(x + '0');
+	_putchar(num + '0');
 	}
 	_putchar('\n');
 }

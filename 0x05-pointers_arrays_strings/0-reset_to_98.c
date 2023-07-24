23f03cf7cc11bb6004b8ae4ae9075c15e6b20487
AnnKamau <3annfaithk@gmail.com>
2023-07-24 00:17:46 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * @n: The value to be checked
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	*n = 98;
+
+}

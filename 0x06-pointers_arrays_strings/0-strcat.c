86442daa67dec1fe63cd2f62a9aebc747520aa50
AnnKamau <3annfaithk@gmail.com>
2023-07-26 09:12:34 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,18 +3,26 @@
  * _strcat - Concatenates two strings using at most
  * @dest: The string to be appended upon
  * @src: The string to be appended to dest
- * @n: The number of bytes from src to be appended to dest
  * Return: A pointer to the result
  */
 
 char *_strcat(char *dest, char *src)
-
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int j;
 
-	while (dest[index++])
-	dest_len++;
-	for (index = 0; src[index] && index < n; index++)
-	dest[dest_len++] = src[index];
+	i = 0;
+	while (dest[i] != '\0')
+	{
+	i++
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }

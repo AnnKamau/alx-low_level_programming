17a8a8746333c3c021afc0472d7496e79c525a19
AnnKamau <3annfaithk@gmail.com>
2023-07-26 10:08:39 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,13 +5,13 @@
  * Return: A pointer to the changed string
  */
 
-char *string_toupper(char *str)
+char *string_toupper(char *)
 {
 	int index = 0;
 
 	while (str[index])
 	{
-	if (str[index] >= 'a' && str[index] <= '2')
+	if (str[index] >= 'a' && str[index] <= 'z')
 	str[index] -= 32;
 	index++;
 	}

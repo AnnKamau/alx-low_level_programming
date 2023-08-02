0b9eb627262682201eddc3a69e7f4a4c408948a2
AnnKamau <3annfaithk@gmail.com>
2023-08-02 00:24:39 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,31 +2,41 @@
 #include <stdio.h>
 
 /**
+ * is_perfect_square - checks if number is a perfect square
+ * @n: first value
+ * @i: second value
+ * Return: void
+ */
+int is_perfect_square(int n, int i)
+{
+	if (i == n)
+	{
+		return (i);
+	}
+	if (i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else
+	{
+		return (is_perfect_square(n, i + 1));
+	}
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: the number
- * Return: -1 if n has no natural square root
+ * @n: The number
+ * Return: void
  */
 
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
-}
-
-/**
- * _sqrt - calculates natural square root
- *  @n: number to calculate
- *  @i: iterate number
- *  Return: The natural square root
- */
-int _sqrt(int n, int i)
-{
-	int sqrt = i * i;
-
-	if (sqrt > n)
+	if (n < 0)
+	{
 		return (-1);
-
-	if (sqrt == n)
-		return (i);
-
-	return (_sqrt(n, i + 1));
+	}
+	return (is_perfect_square(n, 1));
 }

101f60968f096039d754f960cf52bc1d7484e91d
AnnKamau <3annfaithk@gmail.com>
2023-08-10 03:39:09 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -29,7 +29,7 @@ int is_number(char *s)
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
+	int num1, num2, product;
 
 	if (argc != 3)
 	{
@@ -45,9 +45,9 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 
-	mul = num1 * num2;
+	product = num1 * num2;
 
-	printf("%d\n", mul);
+	printf("%d\n", product);
 
 	return (0);
 }

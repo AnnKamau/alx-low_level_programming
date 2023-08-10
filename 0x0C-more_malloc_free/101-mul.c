b46f8ee252daaec156b8d467160663579ab56ebc
AnnKamau <3annfaithk@gmail.com>
2023-08-10 03:28:16 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_number - checks if string is a number
+ * @s: the string to check
+ * Return: 1 if is a number, 0 otherwise
+ */
+
+int is_number(char *s)
+{
+	int j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		if (!isdigit(s[j]))
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: 0 (success), 98 (error)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!is_number(argv[1]) || !is_number(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	mul = num1 * num2;
+
+	printf("%d\n", mul);
+
+	return (0);
+}
--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,4 +1,4 @@
-#incude <unistd.h>
+#include <unistd.h>
 
 /**
  * _putchar - write the character c to stdout
@@ -9,5 +9,5 @@
 
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+       return (write(1, &c, 1));
 }

7b23fb4c21d3f4b87ea24375c31438e75cf2f1a6
AnnKamau <3annfaithk@gmail.com>
2023-08-16 02:52:00 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name
+ * print_name - Prints a name using a pointer function
  * @name: pointer to a character array
  * @f: pointer to a function
  * Return: Nothing

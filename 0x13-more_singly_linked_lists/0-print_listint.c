a7769b8d055c8b0d9206f0fd15512e160d98cd22
AnnKamau <3annfaithk@gmail.com>
2023-08-28 09:52:54 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,11 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t bubble = 0;
-	listint *lead = head;
 
-	while (lead != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", lead->n);
-		lead = lead->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		bubble++;
 	}
 

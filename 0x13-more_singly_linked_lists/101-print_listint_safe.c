e1deb54714ce86e9ad6217be57c7614de19e1a36
AnnKamau <3annfaithk@gmail.com>
2023-08-28 12:00:43 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,52 @@
 #include "lists.h"
 
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
+/**
+ * looped_listint_len - counts number of nodes
+ * @head: pointer to the head of the list to check
+ * Return: if list is not looped - 0. otherwise number of nodes
+ */
+
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *tall, *height;
+	size_t note = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	tall = head->next;
+	height = (head->next)->next;
+
+	while (height)
+	{
+		if (tall == height)
+		{
+			tall = head;
+			while (tall != height)
+			{
+				note++;
+				tall = tall->next;
+				height = height->next;
+			}
+			tall = tall->next;
+
+			while (tall != height)
+			{
+				note++;
+				tall = tall->next;
+			}
+			return (note);
+		}
+		tall = tall->next;
+		height = (height->next)->next;
+	}
+	return (0);
+}
+
+
+
 /**
  * print_listint_safe - prints a listint_t link list safely.
  * @head: pointer to the head of the list.
@@ -8,32 +55,26 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *win, *lose;
-	size_t note = 0;
+	size_t note, ind = 0;
 
-	win = head;
-	lose = head;
+	note = looped_listint_len(head);
 
-	while (win != NULL && lose != NULL && lose->next != NULL)
+	if (note == 0)
 	{
-		win = win->next;
-		lose = lose->next->next;
-
-		printf("[%p] %d\n", (void *)win, win->n);
-		note++;
-
-		if (win == lose)
+		for (; head != NULL; note++)
 		{
-			printf("-> [%p] %d\n", (void *)win, win->n);
-			printf("Infinite Loop detected, exiting...\n");
-			exit(98);
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
-	if (win != NULL)
+	else
 	{
-		printf("[%p] %d\n", (void *)win, win->n);
-		note++;
+		for (ind = 0; ind < note; ind++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
 	}
-
 	return (note);
 }

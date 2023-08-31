fc3e7c695bd229c10361394cc421b7dd2b75bbad
AnnKamau <3annfaithk@gmail.com>
2023-08-31 02:51:32 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -11,3 +15,5 @@ int _putchar(char c);
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+
+#endif

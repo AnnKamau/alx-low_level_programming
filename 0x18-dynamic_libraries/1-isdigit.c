c29cb6d99266ab684e35aed157e2beb27a43a7ef
AnnKamau <3annfaithk@gmail.com>
2023-10-16 09:16:36 -0700

    libdynamic.so

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - function that checks for a digit 0 through 9
+ * @c: The character to be checked
+ * Return: 1 if it is a  digit or 0 if anything else
+ */
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+	{
+
+	return (1);
+	}
+	return (0);
+}
